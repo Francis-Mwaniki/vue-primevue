e9c4438dacc28ad6cae7a0d0c618943baf4729bd
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-09-04 11:16:10 +0300

    testing

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,46 @@
 <template>
   <div>
-    <div class="content">
-      <!-- Your content goes here -->
-      <p>Company Name: {{ companyName }}</p>
-      <p>Profit: {{ profit }}%</p>
-      <p>Entry Price: ${{ entryPrice }}</p>
-      <p>Last Price: ${{ lastPrice }}</p>
-      <p>Shared Code: {{ sharedCode }}</p>
-    </div>
-
-    <!-- Button to generate and share the image -->
-    <button @click="generateImage">Generate Image</button>
-
-    <!-- The generated image will be displayed here -->
-    <img v-if="imageUrl" :src="imageUrl" alt="Generated Image" />
-
-    <!-- Button to share the generated image using Web Share API -->
-    <button v-if="imageUrl" @click="shareImage">Share Image</button>
+    <h1>File Sharing App</h1>
+    <button @click="getImage">Get Image</button>
+    <img :src="imageUrl" />
+    <button @click="shareImage">Share Image</button>
   </div>
 </template>
 
 <script>
-import * as htmlToImage from "html-to-image";
+import { ref } from "vue";
 
 export default {
+  name: "FileSharingApp",
   data() {
     return {
-      companyName: "Beannsofts Inc",
-      profit: +20,
-      entryPrice: 200,
-      lastPrice: 220,
-      sharedCode: "1234567890",
-      imageUrl: null,
+      imageUrl: "",
     };
   },
   methods: {
-    generateImage() {
-      const content = this.$el.querySelector(".content");
-
-      htmlToImage
-        .toPng(content)
-        .then((dataUrl) => {
-          this.imageUrl = dataUrl;
-        })
-        .catch((error) => {
-          console.error("Error generating image:", error);
-        });
+    getImage() {
+      // Get the image from the server
+      this.imageUrl = "https://picsum.photos/200/300";
     },
     shareImage() {
-      if (navigator.share && this.imageUrl) {
+      if (navigator.share) {
+        const shareData = {
+          url: this.imageUrl,
+        };
+
+        // Share the image using the Web Share API
         navigator
-          .share({
-            title: "Generated Image",
-            text: "Check out this generated image!",
-            url: this.imageUrl,
-          })
+          .share(shareData)
           .then(() => {
-            console.log("Shared successfully");
+            alert("Image shared successfully");
           })
           .catch((error) => {
-            console.error("Error sharing:", error);
+            alert("Error sharing image:", error);
           });
       } else {
-        console.log("Web Share API is not supported in this browser.");
+        alert("Web Share API is not supported in this browser");
       }
     },
   },
 };
 </script>
-
-<style scoped>
-/* Add your CSS styles for the content here */
-.content {
-  background-color: #ffffff;
-  padding: 20px;
-  border: 1px solid #cccccc;
-  border-radius: 5px;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-}
-
-button {
-  margin-top: 10px;
-}
-
-img {
-  max-width: 100%;
-  margin-top: 10px;
-}
-</style>
---
+++ src/components/htmlToImage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <!-- Your HTML content here -->
+    <div id="thanku_poster">
+      <!-- Your content to be converted to an image goes here -->
+    </div>
+
+    <button @click="createImage">Create and Share Image</button>
+  </div>
+</template>
+
+<script>
+import { toJpeg } from "html-to-image";
+
+export default {
+  methods: {
+    dataURLtoFile(dataurl, filename) {
+      // Your dataURLtoFile function here
+    },
+    shareFile(file, title, text) {
+      // Your shareFile function here
+    },
+    createImage() {
+      toJpeg(document.getElementById("thanku_poster"), { quality: 0.95 })
+        .then((dataUrl) => {
+          // Assuming you have a method to hide the thanks card
+          this.hideThanksCard();
+
+          const file = this.dataURLtoFile(dataUrl, "thanku_poster.png");
+          this.shareFile(file, "Title", "https://co-aid.in");
+        })
+        .catch((error) => console.log("Error creating image", error));
+    },
+    hideThanksCard() {
+      // Implement a method to hide the thanks card
+    },
+  },
+};
+</script>
