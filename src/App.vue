1d77c26c3dafe6143700621b99075025957800f6
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-09-04 10:14:25 +0300

    testing

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,49 @@
 <template>
   <div>
-    <button @click="shareOnWhatsApp">Share on WhatsApp</button>
+    <button @click="shareImageOnWhatsApp">Share Image on WhatsApp</button>
   </div>
 </template>
 
 <script>
 export default {
   methods: {
-    shareOnWhatsApp() {
-      // Replace 'image_url' with the URL of the image you want to share.
-      const imageUrl =
+    shareImageOnWhatsApp() {
+      // Replace 'your-image.jpg' with the path to your image.
+      const imageSrc =
         "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg";
 
-      // Create a WhatsApp share URL with the image.
-      const whatsappShareUrl = `whatsapp://send?text=${encodeURIComponent(imageUrl)}`;
+      // Convert the image to a Data URI.
+      const imageUri = `data:image/jpeg;base64,${this.getImageData(imageSrc)}`;
+
+      // Create a WhatsApp share URL with the Data URI.
+      const whatsappShareUrl = `whatsapp://send?text=${encodeURIComponent(imageUri)}`;
 
       // Open the WhatsApp mobile app with the share URL.
       window.location.href = whatsappShareUrl;
+    },
+    getImageData(imageSrc) {
+      // You need to implement a function to fetch and convert the image to a Data URI.
+      // This depends on your specific setup, whether you're using a server or client-side code.
+      // Here's a simplified example using FileReader for client-side usage:
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+          const imageData = reader.result.split(",")[1];
+          resolve(imageData);
+        };
+        reader.onerror = (error) => {
+          reject(error);
+        };
+        // Load the image file.
+        fetch(imageSrc)
+          .then((response) => response.blob())
+          .then((blob) => {
+            reader.readAsDataURL(blob);
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      });
     },
   },
 };
